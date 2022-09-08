510f4e3c13c0da20f2c883930c73a6995e442b3e
VladPiatachenko <vlpiatachenko@gmail.com>
2022-09-08 14:20:12 +0300

    droped main

--- Main.java
+++ Main.java
@@ -1,22 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        try{
-            Employee emp=new Employee("Bilbo Baggins");
-            emp.PrintEmpoyee();
-        }catch(FieldLengthLimitException ex){
-            System.out.println(ex.getMessage());
-        }
-        try{
-        Employee emp=new Employee("Bilbo Baggins from Shire");
-        emp.PrintEmpoyee();
-        }catch(FieldLengthLimitException ex){
-            System.out.println(ex.getMessage());
-        }
-        try{
-            Employee emp=new Employee("Frodo Baggins");
-            emp.PrintEmpoyee();
-        }catch(FieldLengthLimitException ex){
-            System.out.println(ex.getMessage());
-        }
+
     }
 }
